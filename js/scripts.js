c3f1d108eac39de94d6d59406a574f7669f5157d
mblanco22 <115679889+mblanco22@users.noreply.github.com>
2024-04-02 15:16:04 -0300

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.5 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function calc() {
+
+  //pega valores inseridos, e substitui virgula por ponto
+  var peso = document.getElementById('peso').value.replace(/,/g, '.');;
+  var altura = document.getElementById('altura').value.replace(/,/g, '.');;
+
+  // valida se os campos não estão vazios
+  if (peso == '' || altura == '') {
+    document.getElementById("message").innerHTML = 'preencha os campos';
+    return false;
+  }
+
+  // calcula IMC
+  var resultado = (peso / (altura * altura));
+  var message = '';
+
+  // verifica situação
+  if (resultado < 17) {
+    message = 'Você está muito abaixo do peso';
+  } else if (resultado >= 17 && resultado <= 18.49) {
+    message = 'Você está abaixo do peso';
+  } else if (resultado >= 18.5 && resultado <= 24.99) {
+    message = 'Seu peso está normal';
+  } else if (resultado >= 25 && resultado <= 29.99) {
+    message = 'Você está acima do peso';
+  } else if (resultado >= 30 && resultado <= 34.99) {
+    message = 'Você tem obesidade nível I';
+  } else if (resultado >= 35 && resultado <= 39.99) {
+    message = 'Você tem Obesidade nível II';
+  } else if (resultado >= 40) {
+    message = 'Você tem Obesidade nível III';
+  }
+
+  // mostra resultado
+  resultado = resultado.toFixed(2).replace(/\./g, ',');
+  document.getElementById("message").innerHTML = 'Seu IMC é : ' + resultado + '<br>' + message;
+}
